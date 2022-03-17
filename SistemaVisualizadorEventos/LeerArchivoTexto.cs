8f201974805b1e4bcce63e67a21d0e4b4331e59a
Jonathan Ortiz <dual.jonathan.ortiz@gmail.com>
2022-03-17 11:43:41 -0600

    Creado el repositorio del proyecto

---
+++ SistemaVisualizadorEventos/LeerArchivoTexto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace VisualizadorDeEventos
+{
+    interface leerArchivo
+    {
+        void leerArchivos();
+    }
+    internal class LeerArchivo : leerArchivo
+    {
+        SepararFechaDeEventos fecha1 = new SepararFechaDeEventos();
+        public void leerArchivos()
+        {
+            //Leer archivo
+            using (StreamReader leer = new StreamReader(@"E:\PROGRAMA DUAL\Programas\Visual Studio 2022\Proyectos\SistemaVisualizadorEventos\eventos.txt"))
+            {
+                while (!leer.EndOfStream)
+                {
+                    string fecha = leer.ReadLine();
+                    Console.WriteLine("\n" + fecha);
+
+                    fecha1.separarFechaDeEventos(fecha);
+                }
+            }
+        }
+    }
+}
